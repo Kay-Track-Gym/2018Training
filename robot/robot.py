39a4e6ad6c5952aa924684573ec3ddffcdc1c8cc
tuffinmuffin <tuffinmuffin@gmail.com>
2019-01-11 18:32:22 -0700

    added new robot.py above module

---
+++ robot/robot.py
@@ -0,0 +1,33 @@
+import team3200
+
+#code to help run the robot
+
+#import sys       
+def exit(retval):
+    pass
+#    sys.exit(retval)
+
+if __name__ == '__main__':
+    try:
+        #patch no exit error if not running on robot
+        try:
+            print(wpilib._impl.main.exit)
+        except:
+            wpilib._impl.main.exit = exit
+            
+        #fixes simulation rerun errors.
+        #todo verify this causes no issues on robot
+        wpilib.DriverStation._reset()
+
+        #patch simulation
+        #we update the simluation files to ours. If we update WPIlib these may break
+        import sim.ui
+        import sim.pygame_joysticks
+        import pyfrc.sim
+        import pyfrc.sim.pygame_joysticks
+        pyfrc.sim.SimUI = sim.ui.SimUI
+        pyfrc.sim.pygame_joysticks.UsbJoysticks = sim.pygame_joysticks.UsbJoysticks
+    except Exception as err:
+            print("Failed to patch runtime. Error", err)
+    
+    wpilib.run(team3200.robot.MyRobot,physics_enabled=True)
